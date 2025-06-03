18ce6ce1008c09e9d4757520a21dea9532143ac4
Ahmed Abdou <Ahmed Abdou@DESKTOP-PL8IF8N>
2025-06-03 16:43:07 +0300

    init project

---
+++ flafel-app/Flafel.Presentation/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace Flafel.Presentation
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+    }
+}
